00400873e2873fe6663c0d406953e68237449d15
Truket Lema <truketlema9489@gmail.com>
2025-09-28 21:25:55 +0300

    Centralize all interfaces in interface/index.ts

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,14 +1,11 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-
-interface PageRouteProps {
-  pageRoute: string;
-}
+import { PageRouteProps } from "@/interface";
 
 export default function Home() {
   const router = useRouter();
 
-  // Imperative routing with useRouter
+  // Imperative routing
   const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
     router.push(pageRoute, undefined, { shallow: false });
   };
